bcc9bac76b16bb6eb1f469b1eaf3dcfdd371b59c
yourious <yourious@github.com>
2024-12-03 23:41:59 +0200

    Course react complated.

--- src/components/EffectSection.jsx
+++ src/components/EffectSection.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState, useCallback } from "react";
 import Button from "./Button/Button";
 import Modal from "./Modal/Modal";
+import useInput from "../hooks/useInput";
 
 export default function EffectSection() {
+  const input = useInput();
   const [modal, setModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = useCallback(async () => {
+    setLoading(true);
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    setUsers(users);
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
   function openModal() {
     setModal(true);
   }
@@ -11,7 +28,9 @@ export default function EffectSection() {
     <section>
       <h3>Effects</h3>
 
-      <Button onClick={openModal}>Open info</Button>
+      <Button style={{ marginBottom: "1rem" }} onClick={openModal}>
+        Open info
+      </Button>
 
       <Modal open={modal}>
         <h3>Hello from Modal</h3>
@@ -22,6 +41,22 @@ export default function EffectSection() {
         </p>
         <Button onClick={() => setModal(false)}>Close modal</Button>
       </Modal>
+
+      {loading && <p>Loading...</p>}
+      {!loading && (
+        <>
+          <input type="text" className="control" {...input} />
+          <ul>
+            {users
+              .filter((user) =>
+                user.name.toLowerCase().includes(input.value.toLowerCase())
+              )
+              .map((user) => (
+                <li key={user.id}>{user.name}</li>
+              ))}
+          </ul>
+        </>
+      )}
     </section>
   );
 }
